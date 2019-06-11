c52c189cfe84de0bdfc08b375ae03dcabf33847e
Jasper Fawkes <design@honeymkr.com>
2019-06-10 21:08:34 -0400

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,11 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: { trackingId: "UA-141803328-3"
+      },
+    },
+    {
       resolve: `gatsby-plugin-alias-imports`,
       options: {
         alias: {
